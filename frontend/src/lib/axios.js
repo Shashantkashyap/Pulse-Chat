277e13709a963a081934802a66d0899916d5ad44
Shashant Kashyap <shashant.kashyap999@gmail.com>
2025-01-22 22:50:43 +0530

    Update axios baseURL for development environment to point to the new API endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://pulse-chat-2.onrender.com",
- baseURL: import.meta.env.MODE === "development" ? "https://pulse-chat-2.onrender.com/api" : "/api",
+ baseURL: import.meta.env.MODE === "development" ? "https://pulse-chat-3.onrender.com" : "/api",
   withCredentials: true,
 });
